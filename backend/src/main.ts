3a09a3e67a82eee07e3bdfbecc8a65d25724224e
hibaouben <souhiba1111@gmail.com>
2026-09-08 20:33:12 +0200

    Fix CORS for production

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,7 +8,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: [
+      'http://localhost:5173',
+      process.env.FRONTEND_URL,
+    ].filter(Boolean),
   });
 
   app.setGlobalPrefix('api');
